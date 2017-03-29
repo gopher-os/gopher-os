c15f27235c09a6c7238d63aa2cb53014146b76ac
Achilleas Anagnostopoulos <achilleas.imap.mbox@gmail.com>
2017-03-29 07:50:29 +0100

    Update rt0 code to check for multiboot support and call kernel.Kmain
    We still keep the required main func in stub.go to prevent the compiler
    from optimizing the code out. We also force the compiler not to inline
    the call to kernel.Kmain so we can find the symbol in the generated .o
    file.

--- kernel/kmain.go
+++ kernel/kmain.go
@@ -1,5 +1,22 @@
 package kernel
 
-// Kmain is invoked by the boot.go and implements the actual kernel entrypoint.
-func Kmain() {
+import (
+	_ "unsafe" // required for go:linkname
+
+	"github.com/achilleasa/gopher-os/kernel/multiboot"
+)
+
+// Kmain is the only Go symbol that is visible (exported) from the rt0 initialization
+// code. This function is invoked by the rt0 assembly code after setting up the GDT
+// and setting up a a minimal g0 struct that allows Go code using the 4K stack
+// allocated by the assembly code.
+//
+// The rt0 code passes the address of the multiboot info payload provided by the
+// bootloader.
+//
+// Kmain is not expected to return. If it does, the rt0 code will halt the CPU.
+//
+//go:noinline
+func Kmain(multibootInfoPtr uint32) {
+	multiboot.SetInfoPtr(uintptr(multibootInfoPtr))
 }
---
+++ stub.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/achilleasa/gopher-os/kernel"
+
+var multibootInfoPtr uint32
+
+// main makes a dummy call to the actual kernel main entrypoint function. It
+// is intentionally defined to prevent the Go compiler from optimizing away the
+// real kernel code.
+//
+// A global variable is passed as an argument to Kmain to prevent the compiler
+// from inlining the actual call and removing Kmain from the generated .o file.
+func main() {
+	kernel.Kmain(multibootInfoPtr)
+}
