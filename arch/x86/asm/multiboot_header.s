4b25afd2f50bc6a6fd26451c986fbc87e8bcda5b
Achilleas Anagnostopoulos <achilleas.imap.mbox@gmail.com>
2017-04-24 08:44:33 +0100

    Request valid EGA console from bootloader

--- arch/x86/asm/multiboot_header.s
+++ arch/x86/asm/multiboot_header.s
@@ -17,9 +17,15 @@ header_start:
 	; 32-bit unsigned sum of zero.
 	dd (1 << 32) - (MAGIC + ARCH + (header_end - header_start))
 
+        ; Console flags tag
 	align 8 ; tags should be 64-bit aligned
-
+	dw 4    ; type
+	dw 0    ; flags
+	dd 12   ; size 
+	dd 0x3  ; kernel supports EGA console
+	
 	; Define graphics mode tag 
+	;align 8 ; tags should be 64-bit aligned
 	;dw 5    ; type
 	;dw 0    ; flags
 	;dd 20   ; size 
@@ -27,10 +33,9 @@ header_start:
 	;dd 25   ; height (pixels or chars)
 	;dd 0    ; bpp (0 for text mode 
 	
-	align 8 ; tags should be 64-bit aligned
-
 	; According to page 6 of the spec, the tag list is terminated by a tag with 
 	; type 0 and size 8
+	align 8 ; tags should be 64-bit aligned
 	dd 0    ; type & flag = 0
 	dd 8    ; size
 header_end:
