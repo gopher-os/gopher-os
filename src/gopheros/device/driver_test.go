d180348116dfe4e45d37956a3c38d2378e929f5d
Achilleas Anagnostopoulos <achilleas.imap.mbox@gmail.com>
2017-07-18 07:04:47 +0100

    Define DriverInfo and registration helpers

---
+++ src/gopheros/device/driver_test.go
@@ -0,0 +1,36 @@
+package device
+
+import (
+	"sort"
+	"testing"
+)
+
+func TestDriverInfoListSorting(t *testing.T) {
+	defer func() {
+		registeredDrivers = nil
+	}()
+
+	origlist := []*DriverInfo{
+		&DriverInfo{Order: DetectOrderACPI},
+		&DriverInfo{Order: DetectOrderLast},
+		&DriverInfo{Order: DetectOrderBeforeACPI},
+		&DriverInfo{Order: DetectOrderEarly},
+	}
+
+	for _, drv := range origlist {
+		RegisterDriver(drv)
+	}
+
+	registeredList := DriverList()
+	if exp, got := len(origlist), len(registeredList); got != exp {
+		t.Fatalf("expected DriverList() to return %d entries; got %d", exp, got)
+	}
+
+	sort.Sort(registeredList)
+	expOrder := []int{3, 2, 0, 1}
+	for i, exp := range expOrder {
+		if registeredList[i] != origlist[exp] {
+			t.Errorf("expected sorted entry %d to be %v; got %v", i, registeredList[exp], origlist[i])
+		}
+	}
+}
